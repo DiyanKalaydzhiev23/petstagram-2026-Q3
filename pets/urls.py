fe882f10101b7d02f0960012428df17e8aae5673
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2026-09-23 19:06:39 +0300

    chore: set up pets app

--- pets/urls.py
+++ pets/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = [
+from django.urls import path, include
+from pets import views
 
+app_name = 'pets'
+
+urlpatterns = [
+    path('add/', views.pet_add_view, name='add'),
+    path('<str:username>/pet/<slug:pet_slug>/', include([
+        path('', views.pet_details_view, name='details'),
+        path('edit/', views.pet_edit_view, name='edit'),
+        path('delete/', views.pet_delete_view, name='delete'),
+    ])),
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def pet_add_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'pets/pet-add-page.html')
+
+
+def pet_details_view(request: HttpRequest, username: str, pet_slug: str) -> HttpResponse:
+    return render(request, 'pets/pet-details-page.html')
+
+
+def pet_edit_view(request: HttpRequest, username: str, pet_slug: str) -> HttpResponse:
+    return render(request, 'pets/pet-edit-page.html')
+
+
+def pet_delete_view(request: HttpRequest, username: str, pet_slug: str) -> HttpResponse:
+    return render(request, 'pets/pet-delete-page.html')
