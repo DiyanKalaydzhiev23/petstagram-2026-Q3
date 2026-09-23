7fdb4f8990d3ed56ac00e9dd68334f5ff3a79ed5
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2026-09-23 18:58:01 +0300

    chore: set up common app

--- common/urls.py
+++ common/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
+from common import views
 
+app_name = 'common'
+
+urlpatterns = [
+    path('', views.home_page_view, name='home')
 ]
--- common/views.py
+++ common/views.py
@@ -1,3 +1,7 @@
+from http.client import HTTPResponse
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page_view(request: HttpRequest) -> HTTPResponse:
+    return render(request, 'common/home-page.html')
