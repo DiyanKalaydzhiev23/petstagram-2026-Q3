ef0a8c84ac268a5b84c5d201d7e8109545698a99
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2026-09-23 19:01:50 +0300

    chore: set up photos app

--- photos/urls.py
+++ photos/urls.py
@@ -1,3 +1,12 @@
-urlpatterns = [
+from django.urls import path, include
+from photos import views
 
+app_name = 'photos'
+
+urlpatterns = [
+    path('add/', views.photo_add_view, name='add'),
+    path('<int:pk>/', include([
+        path('', views.photo_details_view, name='details'),
+        path('edit/', views.photo_edit_view, name='edit'),
+    ])),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def photo_add_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'photos/photo-add-page.html')
+
+
+def photo_details_view(request: HttpRequest, pk: int) -> HttpResponse:
+    return render(request, 'photos/photo-details-page.html')
+
+
+def photo_edit_view(request: HttpRequest, pk: int) -> HttpResponse:
+    return render(request, 'photos/photo-edit-page.html')
